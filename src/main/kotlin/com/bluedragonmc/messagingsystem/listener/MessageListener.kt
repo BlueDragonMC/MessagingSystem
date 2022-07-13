5cb4f3dfcd7974f80f2839cf530c5e0cfca12511
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2022-07-13 11:59:09 -0400

    Add exception handling to prevent closing the connection when there is an exception in a handler

--- src/main/kotlin/com/bluedragonmc/messagingsystem/listener/MessageListener.kt
+++ src/main/kotlin/com/bluedragonmc/messagingsystem/listener/MessageListener.kt
@@ -1,14 +1,22 @@
 package com.bluedragonmc.messagingsystem.listener
 
 import com.bluedragonmc.messagingsystem.message.Message
+import com.bluedragonmc.messagingsystem.message.RPCErrorMessage
 import kotlin.reflect.KClass
 
 abstract class MessageListener<T : Message> {
     abstract val type: KClass<T>
 }
 
-data class PubSubMessageListener<T : Message>(override val type: KClass<T>, val listener: (T) -> Unit) :
-    MessageListener<T>()
+class PubSubMessageListener<T : Message>(override val type: KClass<T>, listener: (T) -> Unit) : MessageListener<T>() {
+    val listener: (T) -> Unit = { message ->
+        kotlin.runCatching { listener(message) }.onFailure { e -> e.printStackTrace() }
+    }
+}
 
-data class RPCMessageListener<T : Message>(override val type: KClass<T>, val listener: (T) -> Message) :
-    MessageListener<T>()
+class RPCMessageListener<T : Message>(override val type: KClass<T>, listener: (T) -> Message) : MessageListener<T>() {
+    val listener: (T) -> Message = { message ->
+        kotlin.runCatching { listener(message) }
+            .getOrElse { e -> RPCErrorMessage("Error in RPC handler: ${e::class.simpleName}: ${e.message}") }
+    }
+}
